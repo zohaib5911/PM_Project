ab5335a4d6964f345de0de83cd60c2402f4b7190
M Zohaib Sajjad <149583085+zohaib5911@users.noreply.github.com>
2025-10-17 02:16:49 +0500

    Add config.py

---
+++ PM_Project/config.py
@@ -0,0 +1,6 @@
+# Configuration settings for the Flask app
+
+class Config:
+    SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
